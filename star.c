afab648957417450e54701d7333ba1aa6bc57e55
Elmono56 <hidalgo.pablo56@gmail.com>
2024-05-13 16:45:26 -0600

    first body of STar

---
+++ star.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BLOCK_SIZE 256 * 1024 // 256 KB
+
+typedef struct{
+    String name;
+} FileE;
+
+typedef struct{
+} FAT;
+
+
+void create_file(){
+
+};
+
+void extract_files(){
+
+};
+
+void delete_files(){
+
+};
+
+void update_files(){
+
+};
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+};
